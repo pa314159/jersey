77be39b395c31da3f62e5f3da68705964279990b
pappy <pappy.stanescu@gmail.com>
2019-10-27 13:07:31 +0100

    generic return type

--- ext/microprofile/mp-rest-client/src/main/java/org/glassfish/jersey/microprofile/restclient/MethodModel.java
+++ ext/microprofile/mp-rest-client/src/main/java/org/glassfish/jersey/microprofile/restclient/MethodModel.java
@@ -24,11 +24,13 @@ import java.lang.reflect.Parameter;
 import java.lang.reflect.ParameterizedType;
 import java.lang.reflect.Proxy;
 import java.lang.reflect.Type;
+import java.lang.reflect.TypeVariable;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.HashSet;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
@@ -515,7 +517,96 @@ class MethodModel {
         private Builder(InterfaceModel interfaceModel, Method method) {
             this.interfaceModel = interfaceModel;
             this.method = method;
+
+            Type[] actualType = resolveActualTypeArgs(interfaceModel.getRestClientClass(), method.getDeclaringClass());
+
+            if (actualType != null && actualType.length > 0) {
+                this.returnType = new GenericType<>(actualType[0]);
+            } else {
+                this.returnType = new GenericType<>(method.getGenericReturnType());
+            }
+
             filterAllInterceptorAnnotations();
+        }
+
+        /**
+         * Resolves the actual generic type arguments for a base class, as viewed from a subclass or implementation.
+         *
+         * @param <T> base type
+         * @param actual class or interface subclassing or extending the base type
+         * @param base base class
+         * @param actualArgs the actual type arguments passed to the offspring class
+         * @return actual generic type arguments, must match the type parameters of the offspring class. If omitted, the
+         * type parameters will be used instead.
+         */
+        public static Type[] resolveActualTypeArgs(Class<?> actual, Class<?> base, Type... actualArgs) {
+            assert actual != null;
+            assert base != null;
+            assert actualArgs.length == 0 || actualArgs.length == actual.getTypeParameters().length;
+
+            //  If actual types are omitted, the type parameters will be used instead.
+            if (actualArgs.length == 0) {
+                actualArgs = actual.getTypeParameters();
+            }
+            // map type parameters into the actual types
+            Map<String, Type> typeVariables = new HashMap<String, Type>();
+            for (int i = 0; i < actualArgs.length; i++) {
+                TypeVariable<?> typeVariable = (TypeVariable<?>) actual.getTypeParameters()[i];
+                typeVariables.put(typeVariable.getName(), actualArgs[i]);
+            }
+
+            // Find direct ancestors (superclass, interfaces)
+            List<Type> ancestors = new LinkedList<>();
+            if (actual.getGenericSuperclass() != null) {
+                ancestors.add(actual.getGenericSuperclass());
+            }
+            for (Type t : actual.getGenericInterfaces()) {
+                ancestors.add(t);
+            }
+
+            // Recurse into ancestors (superclass, interfaces)
+            for (Type type : ancestors) {
+                if (type instanceof Class<?>) {
+                    // ancestor is non-parameterized. Recurse only if it matches the base class.
+                    Class<?> ancestorClass = (Class<?>) type;
+                    if (base.isAssignableFrom(ancestorClass)) {
+                        Type[] result = resolveActualTypeArgs(ancestorClass, base);
+                        if (result != null) {
+                            return result;
+                        }
+                    }
+                }
+                if (type instanceof ParameterizedType) {
+                    // ancestor is parameterized. Recurse only if the raw type matches the base class.
+                    ParameterizedType parameterizedType = (ParameterizedType) type;
+                    Type rawType = parameterizedType.getRawType();
+                    if (rawType instanceof Class<?>) {
+                        Class<?> rawTypeClass = (Class<?>) rawType;
+                        if (base.isAssignableFrom(rawTypeClass)) {
+
+                            // loop through all type arguments and replace type variables with the actually known types
+                            List<Type> resolvedTypes = new LinkedList<Type>();
+                            for (Type t : parameterizedType.getActualTypeArguments()) {
+                                if (t instanceof TypeVariable<?>) {
+                                    Type resolvedType = typeVariables.get(((TypeVariable<?>) t).getName());
+                                    resolvedTypes.add(resolvedType != null ? resolvedType : t);
+                                } else {
+                                    resolvedTypes.add(t);
+                                }
+                            }
+
+                            Type[] result = resolveActualTypeArgs(rawTypeClass, base, resolvedTypes.toArray(new Type[] {}));
+
+                            if (result != null) {
+                                return result;
+                            }
+                        }
+                    }
+                }
+            }
+
+            // we have a result if we reached the base class.
+            return actual.equals(base) ? actualArgs : null;
         }
 
         private void filterAllInterceptorAnnotations() {
@@ -554,6 +645,9 @@ class MethodModel {
          * @param returnType Method return type
          */
         private void returnType(Type returnType) {
+            if (returnType instanceof TypeVariable) {
+                interfaceModel.getRestClientClass().getTypeParameters();
+            }
             if (returnType instanceof ParameterizedType
                     && CompletionStage.class.isAssignableFrom((Class<?>) ((ParameterizedType) returnType).getRawType())) {
                 this.returnType = new GenericType<>(((ParameterizedType) returnType).getActualTypeArguments()[0]);
